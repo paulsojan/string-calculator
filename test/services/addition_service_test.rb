461bed6eb00c43a10761647b5a1ac24c1aea2041
Paul Sojan <paul.eliassojan@gmail.com>
2024-08-19 00:23:13 +0530

    Added test to AdditionService to allow delimiter with length more than one char

--- test/services/addition_service_test.rb
+++ test/services/addition_service_test.rb
@@ -44,4 +44,9 @@ class AdditionServiceTest < ActiveSupport::TestCase
     sum = AdditionService.new("//[*][%]\n1*2%3").process
     assert_equal 6, sum
   end
+
+  def test_should_allow_multiple_delimiters_with_length_more_than_one_char
+    sum = AdditionService.new("//[**][%%]\n1**2%%3**4").process
+    assert_equal 10, sum
+  end
 end
